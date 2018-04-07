d2c4caa40560b2613018300587aa61ee7813652e
Thanawat Petchsri <thanawat.petchsri@gmail.com>
2018-04-07 11:20:25 +0700

    Login process

--- web/Components/Login/Login.js
+++ web/Components/Login/Login.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import {PropTypes} from 'prop-types';
+import { PropTypes } from 'prop-types';
+import axios from 'axios';
 
 import svgLogo from '../../logo.svg';
 import './Login.scss';
+import Axios from 'axios';
 
 class Login extends React.Component {
     constructor(props) {
@@ -10,7 +12,9 @@ class Login extends React.Component {
 
         this.state = {
             inputEmail: '',
-            ipputPassword: ''
+            ipputPassword: '',
+            isSubmitting: false,
+            error: ''
         };
 
         this.doSetInputEmail = this.doSetInputEmail.bind(this);
@@ -31,31 +35,111 @@ class Login extends React.Component {
     }
 
     doSubmit() {
-
+        this.setState({
+            isSubmitting: true,
+            error: ''
+        }, () => {
+            const config = {
+                url: 'http://localhost:3000/api/login',
+                method: 'post',
+                data: {
+                    email: this.state.inputEmail,
+                    password: this.state.ipputPassword
+                }
+            };
+            const request = axios
+                .request(config)
+                .then(response => {
+                    this.setState({
+                        isSubmitting: false
+                    });
+                    const status = response.status;
+                    const data = response.data;
+                    if (status === 200) {
+                        alert('Login Successed');
+                    }
+                })
+                .catch(error => {
+                    this.setState({
+                        isSubmitting: false
+                    });
+                    const response = error.response;
+                    const status = response.status;
+                    const data = response.data;
+                    if (status === 401) {
+                        this.setState({
+                            error: data.msg
+                                ? data.msg
+                                : 'Something wrong with login process'
+                        });
+                    } else {
+                        this.setState({
+                            error: data.msg
+                                ? data.msg
+                                : 'Something wrong with login process'
+                        });
+                    }
+                });
+        });
     }
 
     render() {
+        const {isSubmitting} = this.state;
+        const formCssClass = ['page--login__form'];
+        if (isSubmitting) {
+            formCssClass.push('loading');
+        }
+
         return (
             <div className="page--login">
                 <div className="page--login__panel">
-                    <div className="logo">
-                        <img className="logo-img" src={svgLogo} />
-                    </div>
-                    <div className="page--login__form">
+                    <form
+                        className={formCssClass.join(' ')}
+                        onSubmit={e => {
+                            e.preventDefault();
+                            this.doSubmit(e);
+                        }}
+                    >
+                        <div className="logo">
+                            <img className="logo-img" src={svgLogo} />
+                        </div>
                         <div className="input-field">
                             <label>E-main address</label>
-                            <input type="text" onChange={(e) =>this.doSetInputEmail(e.target.value)} />
+                            <input
+                                type="text"
+                                disabled={isSubmitting}
+                                onChange={e =>
+                                    this.doSetInputEmail(e.target.value)
+                                }
+                            />
                         </div>
                         <div className="input-field">
                             <label>Password</label>
-                            <input type="password" onChange={(e) =>this.doSetInputPassword(e.target.value)} />
+                            <input
+                                type="password"
+                                disabled={isSubmitting}
+                                onChange={e =>
+                                    this.doSetInputPassword(e.target.value)
+                                }
+                            />
                         </div>
+                        {this.state.error ? (
+                            <div className="error-message">
+                                <span>{this.state.error}</span>
+                            </div>
+                        ) : null}
                         <div className="btn-group">
-                            <button onClick={() => this.doSubmit()}>
+                            <button
+                                type="submit"
+                                disabled={isSubmitting}
+                                onClick={() => {
+                                    this.doSubmit();
+                                }}
+                            >
                                 <span>Sign in</span>
                             </button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         );
